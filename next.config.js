67f6b5a45ab9c2f4cfc061f497c3a393070dd6ed
Aruseli <llingveri@gmail.com>
2021-09-21 21:42:02 +0000

    gh-pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  basePath: isProd ? '/store' : '',
+  basePath: isProd ? '/adaptive' : '',
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/Aruseli/adaptive/main/docs/' : '',
 }
