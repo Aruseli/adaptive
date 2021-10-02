598befc5312be3c77a50985cb477e3505ed95c09
Anastasia <llingveri@gmail.com>
2021-10-03 02:00:49 +0300

    https://github.com/Aruseli/adaptive/issues/12

--- imports/components/catalog-placeholder.tsx
+++ imports/components/catalog-placeholder.tsx
@@ -1,15 +1,32 @@
 import Box from '@material-ui/core/Box';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import React from 'react';
-import { CatalogViewVariants } from './catalog-view-variants';
-import { DisplayOptionsBar } from './header/app-bar-display-options';
+import { AppBarDisplayOptionsBar } from './header/app-bar-display-options';
 import { ProductCard } from './product-card/product-card';
 
 
-export const CatalogPlaceholder = React.memo(() => {
-  return (<><DisplayOptionsBar>
-        <CatalogViewVariants />
-      </DisplayOptionsBar>
+const useStyles = makeStyles(theme => ({
+
+})) 
+
+export const CatalogPlaceholder = React.memo<any>(() => {
+
+  const chips = [
+    {
+      id:'a',
+      title: 'красное',
+    },
+    {
+      id:'b',
+      title: 'Италия',
+    }
+  ]
+
+  return (<><AppBarDisplayOptionsBar selectedFilterOptions={chips}/>
       <Box component='section'>
+        <ProductCard 
+          title='Вино Tenuta di Trecciano, "Terra Rossa" Senesi Riserva DOCG, 2015'	
+        />
         <ProductCard 
           title='Вино Tenuta di Trecciano, "Terra Rossa" Senesi Riserva DOCG, 2015'	
         />
